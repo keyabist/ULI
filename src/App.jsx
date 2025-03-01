20c7f354b98972396be6bf09022c1f6c983ef9d9
Mihir <mihiradhikari24@gmail.com>
2025-03-01 23:19:55 +0530

    react : connect dashboard with login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 import Login from './Login'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/navbar';
+import LenderDashboard from './components/LenderDashboard';
+import BorrowerDashboard from './components/BorrowerDashboard';
+import EditProfileForm from './components/EditProfileForm';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/lenderDashboard" element={<LenderDashboard />} />
+          <Route path="/borrowerDashboard" element={<BorrowerDashboard />} />
+          <Route path="/edit-profile" element={<EditProfileForm />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
